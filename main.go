6c7d0853878519ed6a5545fbd236ab959c9ce4ab
MagosDragoon29 <carpenter.evan29@gmail.com>
2026-05-08 18:53:41 -0400

    added index and the code to reach it when accessing the server.

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "net/http"
 
 func main() {
-	mux := http.NewServeMux()
+	mux := http.NewServeMux() // ServeMux routes requests. Empty mux = 404
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := &http.Server{
-		Handler: mux,
-		Addr:    ":8080",
+		Handler: mux,     // Handler is the brain that routes all server requests
+		Addr:    ":8080", // [host]:[port], :8080 means all interfaces listen on port 8080
 	}
 	server.ListenAndServe()
 }
